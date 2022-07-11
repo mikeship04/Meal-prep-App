5d9aeac0e4d0e78a0999d454890f5ccf7ca2588e
Mike Shippy <mikeship04@gmail.com>
2022-07-11 15:30:20 -0700

    added styling

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import SearchBar from '../SearchBar/SearchBar'
+import NavBar from './NavBar.css'
 
 function Navbar() {
   return (
-    <div>
+    <div className={NavBar}>
         Navbar
         <SearchBar />
     </div>
