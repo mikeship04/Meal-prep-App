db86a3d7b6042e231dfcf1d3d82da7dbfc861290
Mike Shippy <mikeship04@gmail.com>
2022-07-11 13:32:23 -0700

    added quality of life

--- src/Components/ShoppingListForm/ShoppingListForm.js
+++ src/Components/ShoppingListForm/ShoppingListForm.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function ShoppingListForm() {
   return (
-    <div>ShoppingListForm</div>
+    <div>ShoppingListForm
+      <form>
+        <label for="Ingredient">Add Ingredient</label>
+        <input type="text" name="Ingredient"></input>
+        <label for="Quantity">Add Quantity</label>
+        <input type="text" name="Quantity"></input>
+      </form>
+    </div>
   )
 }
 
