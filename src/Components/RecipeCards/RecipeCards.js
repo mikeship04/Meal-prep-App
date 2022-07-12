0d5bd2157569aca40e11f2b5ae893a7526e4998d
Mike Shippy <mikeship04@gmail.com>
2022-07-11 21:09:18 -0700

    added more styling via material ui

--- src/Components/RecipeCards/RecipeCards.js
+++ src/Components/RecipeCards/RecipeCards.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Instructions from '../Instructions'
 import Recipecards from './Recipecards.css'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
+import Box from '@mui/material/Box'
 
 // what we want to display - cook_time_minutes, description, name,
 // prep_time_minutes, thumbnail_url, yields 
@@ -25,19 +29,31 @@ function RecipeCards({recipe}) {
       })
 
   return (
-    <div className="Recipecards">
-        <h2>{name}</h2>
-        <h3>Description:  {description}
-        </h3>
-        <img className="recipe-image" alt={'not found'} src={thumbnail_url}/>
-        <h4>prep time: {prep_time_minutes} minutes</h4>
-        <h4>cook time: {cook_time_minutes} minutes</h4>
-        <h4>yields: {yields}</h4>
-        <ol>instructions
-            <li>{recipeInstructions}</li>
-        </ol>
-    </div>
+  <Grid item xs={4}> 
+      <Paper elevation={3} className="Recipecards">
+          <img className="recipe-image" alt={name} src={thumbnail_url}/>
+            <Box paddingX={1}>
+              <Typography variant="h5" component="h2">{name}</Typography>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                  }}>
+                  <Typography variant="body2" component="p">
+                    Description:  {description}
+                  </Typography>
+                </Box>
+            </Box>
+            <Box paddingX={1}><Typography variant="subtitle1">prep time: {prep_time_minutes} minutes</Typography></Box>
+            <Box paddingX={1}><Typography variant="subtitle1">cook time: {cook_time_minutes} minutes</Typography></Box>
+            <Box paddingX={1}><Typography variant="subtitle1">yields: {yields}</Typography></Box>
+      </Paper>
+  </Grid>
   )
 }
 
 export default RecipeCards
+
+          // <ol>instructions
+          //     <li>{recipeInstructions}</li>
+          // </ol>
